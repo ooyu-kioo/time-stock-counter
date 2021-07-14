0b439ab260802b6ba71906154031dcd0d5ea1eac
yuki.tashiro <43487435+ooyu-kioo@users.noreply.github.com>
2021-07-14 17:08:47 +0900

    add RecoilRoot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil"
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Component {...pageProps} />
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
   );
 };
 
